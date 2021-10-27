ba3ef10d30ad3aca694e3a7c27b7e2927920893b
shahrin2021 <mdshahrinbinselim1991@gmail.com>
2021-10-27 17:58:30 +0600

    onn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const ObjectId= require('mongodb').ObjectId;
 const app= express();
 const cors = require('cors')
-const port = 5000;
+const port =process.env.PORT ||  5000;
 
 
 // pass :    uL76fuZKO32EH76P 
